0db1dfd4c5b20fbc6ec6c71b5739cbdc7256f4f2
Nicolas Muciaccia <nicolasmuciaccia.dev@gmail.com>
2025-06-17 11:21:49 -0300

    Implementando as posições possíveis de movimento

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -12,6 +12,13 @@ try
         Console.WriteLine();
         Console.Write("Origem: ");
         Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+
+        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+        Console.WriteLine();
         Console.Write("Destino: ");
         Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
 
--- xadrez/xadrez/Rei.cs
+++ xadrez/xadrez/Rei.cs
@@ -9,6 +9,61 @@ namespace xadrez
         {
         }
 
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Verificar acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            if(tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar nordeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar sudeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar sudoeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            // Verificar noroeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+                mat[pos.linha, pos.coluna] = true;
+
+            return mat;
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tabuleiro.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
         public override string ToString()
         {
             return "R";
